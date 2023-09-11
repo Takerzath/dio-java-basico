ddaa6ab9c76bb0beddaac009ca9cffbf617bc571
takerzath <thiagobiomedic@hotmail.com>
2023-09-11 10:32:02 -0300

    commit criado após aula de métodos e variáveis

--- trilha java básico/java-anatomia-de-classes/src/MinhaClasse.java
+++ trilha java básico/java-anatomia-de-classes/src/MinhaClasse.java
@@ -2,9 +2,17 @@ public class MinhaClasse {
 
    public static void main (String [] args) {
      
-      
+      String primeiroNome = "Thiago";
+      String segundoNome = "Rafael";
 
+      String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
 
-   }
+      System.out.println(nomeCompleto);
+   }   
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+   return "Resultado do método " + primeiroNome.concat (" ").concat(segundoNome);
+}
+   
 
 }
